d7269dcbe0e1e396df6cfdf15a4fc8019f97826e
John Doty <jrhdoty@gmail.com>
2013-07-10 21:59:19 -0700

    using filesync and buffer

--- web.js
+++ web.js
@@ -1,10 +1,16 @@
+var fs = require('fs');
+var buf = require('buffer');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var output = fs.readFileSync('Index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
