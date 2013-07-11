5af8b7f1b8642d78b791701820e7dcf7c31539f7
John Doty <jrhdoty@gmail.com>
2013-07-10 22:04:01 -0700

    removed require buffer line

--- web.js
+++ web.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var buf = require('buffer');
 
 var express = require('express');
 
@@ -10,7 +9,6 @@ var output = fs.readFileSync('Index.html');
 app.get('/', function(request, response) {
   response.send(output.toString());
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
